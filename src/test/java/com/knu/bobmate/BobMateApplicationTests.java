8518f9051db3b4de6b463d91e1ba914bdc51e40a
devMuscle <hongsbien@naver.com>
2023-12-02 16:51:00 +0900

    setting: project setting

---
+++ src/test/java/com/knu/bobmate/BobMateApplicationTests.java
@@ -0,0 +1,13 @@
+package com.knu.bobmate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BobMateApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
