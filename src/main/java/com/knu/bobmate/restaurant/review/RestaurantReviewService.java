5ffa2aff1a222d68239a9d6ca398a663492fd48a
devMuscle <hongsbien@naver.com>
2023-12-04 00:33:41 +0900

    Feat: 식당 리뷰 전체 조회

--- src/main/java/com/knu/bobmate/restaurant/review/RestaurantReviewService.java
+++ src/main/java/com/knu/bobmate/restaurant/review/RestaurantReviewService.java
@@ -1,2 +1,18 @@
-package com.knu.bobmate.restaurant.review;public class RestaurantReviewService {
+package com.knu.bobmate.restaurant.review;
+
+import com.knu.bobmate.restaurant.review.dto.RestaurantReviewDto;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@RequiredArgsConstructor
+@Service
+public class RestaurantReviewService {
+
+    private final RestaurantReviewRepository restaurantReviewRepository;
+
+    public List<RestaurantReviewDto> viewAllRestaurantReviewsBy(int restaurantId) {
+        return restaurantReviewRepository.findAllBy(restaurantId);
+    }
 }
--- src/main/java/com/knu/bobmate/restaurant/review/dto/RestaurantReviewDto.java
+++ src/main/java/com/knu/bobmate/restaurant/review/dto/RestaurantReviewDto.java
@@ -1,4 +1,12 @@
 package com.knu.bobmate.restaurant.review.dto;
 
+import lombok.Data;
+
+@Data
 public class RestaurantReviewDto {
+    private int id;
+    private int score;
+    private String description;
+    private int restaurantId;
+    private int participantId;
 }
